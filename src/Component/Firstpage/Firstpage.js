7d2f8e974155d8b68720846bef823c85f369ae5b
@kumarnadi22 <kumarnadi22@gmail.com>
2022-05-01 02:33:58 +0530

    height added

--- src/Component/Firstpage/Firstpage.js
+++ src/Component/Firstpage/Firstpage.js
@@ -27,7 +27,7 @@ const Firstpage = () => {
     <div className='container-fluid'>
       <div className='row'>
         <div className='col-md-3 '>
-        <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{height: "150vh"}}>
+        <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style={{height: "100vh"}}>
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
       <svg class="bi me-2" width="40" height="32"><use></use></svg>
       <span class="fs-4">Choose your car</span>
